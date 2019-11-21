c44a0e70fa4c7bbbd8b3ed4ed11332cf798c7220
JianfeiMa <1017033681@qq.com>
2019-11-21 16:55:37 +0800

    optimizing

--- app/src/main/java/com/buyuphk/mqttdemo/MainActivity.java
+++ app/src/main/java/com/buyuphk/mqttdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.buyuphk.mqttdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * description: MQTT = Message Queuing Telemetry Transport 消息队列遥测传输协议
@@ -18,9 +23,10 @@ import android.widget.TextView;
  * motto: 勇于向未知领域探索
  */
 public class MainActivity extends AppCompatActivity implements IGetMessageCallBack {
-
+    private ConstraintLayout constraintLayout;
     private TextView textView;
     private Button button;
+    private EditText editText;
     private MyServiceConnection serviceConnection;
     private MQTTService mqttService;
 
@@ -28,9 +34,10 @@ public class MainActivity extends AppCompatActivity implements IGetMessageCallBa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        constraintLayout = findViewById(R.id.root);
         textView = (TextView) findViewById(R.id.activity_main_text_view);
         button = (Button) findViewById(R.id.activity_main_button);
+        editText = editText = findViewById(R.id.activity_main_edit_text_publish_topic_message);
 
         serviceConnection = new MyServiceConnection();
         serviceConnection.setIGetMessageCallBack(MainActivity.this);
@@ -42,7 +49,12 @@ public class MainActivity extends AppCompatActivity implements IGetMessageCallBa
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MQTTService.publish("Use Message Queuing Telemetry Transport Publish Message");
+                String topicMessage = editText.getText().toString();
+                if (!topicMessage.equals("")) {
+                    MQTTService.publish(topicMessage);
+                } else {
+                    Toast.makeText(mqttService, "不能发布空的主题消息", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -50,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements IGetMessageCallBa
     @Override
     public void setMessage(String message) {
         textView.setText(message);
+        //Toast.makeText(mqttService, "收到新的订阅主题消息：" + message, Toast.LENGTH_SHORT).show();
+        Snackbar.make(constraintLayout, "收到新的订阅主题消息：" + message, Snackbar.LENGTH_SHORT).show();
         mqttService = serviceConnection.getMqttService();
         mqttService.toCreateNotification(message);
     }
